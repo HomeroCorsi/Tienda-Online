4f0ddf2bb3af46a756e8f82f98370f33c6c4fa15
homeroCorsi <hscorsi@hotmail.com>
2023-01-12 18:54:00 -0300

    primera versión

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,29 @@
 // pages/_app.js
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider, Container, Image, VStack, Text, Heading, Box, Divider } from '@chakra-ui/react'
 import { AppProps } from 'next/app'
+import '../styles/global.css';
+import theme from '../theme';
 
 const App: React.FC<AppProps> = ({ Component, pageProps }) => {
   return (
-    <ChakraProvider>
-      <Component {...pageProps} />
+    <ChakraProvider theme={theme}>
+      <Box padding={4}>
+      <Container
+          backgroundColor="white"
+          boxShadow='md'
+          marginY={4}
+          maxWidth='container.xl'
+          padding={4}
+      >
+        <VStack marginBottom={6} padding={4}>
+          <Image borderRadius={9999} src='//placehold.it/128x128'/>
+          <Heading>Almacen</Heading>
+          <Text>El almacen de homero</Text>
+        </VStack> 
+        <Divider marginY={6}/>
+        <Component {...pageProps} />
+      </Container>
+      </Box>
     </ChakraProvider>
   )
 }
